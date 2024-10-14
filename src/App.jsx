9da6f4b735b850139474de65c65e7287b1cb22d8
deepakdk11 <deepakkumar545424@gmail.com>
2024-10-14 23:53:22 +0530

    Create NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <NavBar />
     </div>
+    
   )
 }
 
